9535e891870d4715769d04a2806903a8f64b938a
掛川裕幸 <kakegawahiroyuki@kakegawahiroyukinoMacBook-Pro.local>
2021-11-07 17:38:40 +0900

    [start]JS

---
+++ app.js
@@ -0,0 +1,32 @@
+let hello = 'Hello World';
+let int1 = 1;
+//負数を代入する
+let int2 = -10;
+//小数点を代入する
+let float1 = 3.14;
+//alert(float1);
+
+//alert(4+3);
+//alert(8-5);
+//掛け算
+//alert(2*6);
+//割り算
+// alert(10/2);
+
+// alert('Hello'+'World');
+
+let str1 = 'Hello';
+let str2 = 'World';
+// alert(str1+str2);
+
+
+let orange = 100;
+let apple = 120;
+
+// if(orange < apple){
+//   alert('みかんの値段がりんごより安い');
+// }else if(orange == apple){
+//   alert('みかんとりんごが同じ値段');
+// }else{
+//   alert('みかんの値段がりんごより高い');
+// }
